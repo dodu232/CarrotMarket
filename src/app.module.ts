95416a1778a2f720e5c9cec75f2544735b547c73
bo <seobg@sj-enm.com>
2024-07-22 14:41:09 +0900

    feat: user 도메인 추가, env파일 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      name: 'user',
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
